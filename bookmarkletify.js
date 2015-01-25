99566369a886665b212364cfd73e5fdd487d9505
olizilla <oli@zilla.org.uk>
2015-01-25 20:21:31 +0000

    Simplify with wrap-stream

--- bookmarkletify.js
+++ bookmarkletify.js
@@ -1,32 +1,19 @@
 var urlencode = require('urlencode-stream')
 var uglify = require('uglify-stream')
-var through = require('through2')
+var wrap = require('wrap-stream')
 var pumpify = require('pumpify')
 var ugly = require('uglify-js')
 
 var prefix = 'javascript:(function(){;'
 var suffix = ';})()'
 
+// minify -> url encode -> wrap in a bookmarklet
 module.exports = function () {
-  function pre (chunk, enc, cb) {
-    var first = true
-    if (first) {
-      first = false
-      cb(null, prefix + chunk)
-    } else {
-      cb(null, chunk)
-    }
-  }
-
-  function post (cb) {
-    this.push(suffix)
-    cb()
-  }
-
-  var pipline = pumpify(uglify(), new urlencode(), through(pre, post))
+  var pipline = pumpify(uglify(), new urlencode(), wrap(prefix, suffix))
   return pipline
 }
 
+// Same process, just sync / non-streaming
 module.exports.sync = function (src) {
   var ast = ugly.parse(src)
   ast.figure_out_scope()
